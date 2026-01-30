1583106e5c0f3e7313e5eed250aa8b751fca1594
Angel <palaciosangel22@outlook.com>
2026-01-30 15:50:13 -0600

    Se agrego un nuevo formulario en el que existen 2 botones, uno para realizar una suma de 2 numeros enteros, y otro para eliminar los valores de los textbox.

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            variableA = new TextBox();
+            variableB = new TextBox();
+            btnCalcular = new Button();
+            btnReset = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Variable A:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Variable B:";
+            // 
+            // variableA
+            // 
+            variableA.Location = new Point(64, 103);
+            variableA.Name = "variableA";
+            variableA.Size = new Size(199, 23);
+            variableA.TabIndex = 2;
+            // 
+            // variableB
+            // 
+            variableB.Location = new Point(64, 203);
+            variableB.Name = "variableB";
+            variableB.Size = new Size(199, 23);
+            variableB.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(324, 91);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(115, 45);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(324, 191);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(115, 50);
+            btnReset.TabIndex = 5;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(btnCalcular);
+            Controls.Add(variableB);
+            Controls.Add(variableA);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCalculadora";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox variableA;
+        private TextBox variableB;
+        private Button btnCalcular;
+        private Button btnReset;
+    }
+}
---
+++ Calculadora/frmCalculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int a = 0, b = 0, resultado = 0;
+
+                a = Convert.ToInt32(variableA.Text);
+                b = int.Parse(variableB.Text);
+
+                resultado = a + b;
+
+                MessageBox.Show("El resultado de la suma es: " + resultado.ToString(), "SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                reset();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo realizar la operacion.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+
+
+
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void reset()
+        {
+             
+            variableA.ResetText();
+            variableB.ResetText();
+            variableA.Focus();
+
+        }
+    }
+}
