0232d3bb68f9b403f7242680eee34f3d8ca3aad9
Angel <palaciosangel22@outlook.com>
2026-02-09 15:49:19 -0600

    agregamos una alarma, y descubrimos como agregar un sonido para que suene cada que llegue a la hora estipulada.

--- Calculadora/Formularios/frmTemporizadores.cs
+++ Calculadora/Formularios/frmTemporizadores.cs
@@ -5,11 +5,15 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Media;
+using CSCore.SoundOut;
 
 namespace Calculadora.Formularios
 {
     public partial class frmTemporizadores : Form
     {
+        string alarma1 = "";
         public frmTemporizadores()
         {
             InitializeComponent();
@@ -18,6 +22,17 @@ namespace Calculadora.Formularios
         private void tmrHora_Tick(object sender, EventArgs e)
         {
             lblHora.Text = DateTime.Now.ToLongTimeString();
+            if (lblHora.Text == alarma1)
+            {
+                SoundPlayer player = new SoundPlayer(@"C:\Users\Angel\source\repos\Calculadora\Calculadora\Sonidos\500 caballos de fuerza.wav");
+                player.Play();  
+            }
+        }
+
+        private void alarma1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alarma1 = Interaction.InputBox("Ingrese la Hora ", "Sistema","00:00:00 x.x");
+
         }
     }
 }
